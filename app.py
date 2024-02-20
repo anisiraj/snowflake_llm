f9173c4539200c23043d6868564622d556172dfd
AnirbanDesktop <abhattacharje@socivolta.com>
2024-02-19 21:09:24 -0500

    created chat function

---
+++ app.py
@@ -0,0 +1,19 @@
+import random
+import gradio as gr
+from snowflake_llm.agent import get_query_chain
+
+
+def respond(query, history):
+    chain, query_writer_chain = get_query_chain()
+    try:
+        res = chain.invoke({"question": query})
+        return res
+    except Exception as e:
+        print(e)
+        return f"Sorry, I had error executing your query {e}."
+    
+
+
+
+if __name__ == "__main__":  
+    gr.ChatInterface(respond).launch()
