6093b2bb4564c80e9c576a4db9d2ed096454ab72
AnirbanDesktop <abhattacharje@socivolta.com>
2024-02-19 22:42:34 -0500

    local variables are set in docker run command, works in docker

--- docker_launch.sh
+++ docker_launch.sh
@@ -1,1 +1,9 @@
-docker run -p 7860:7860 snowflake_llm
+
+docker run -p 7860:7860\
+    --env OPENAI_API_KEY\
+    --env SNOWFLAKE_USERNAME\
+    --env SNOWFLAKE_PASSWORD\
+    --env SNOWFLAKE_ROLE\
+    --env SNOWFLAKE_WAREHOUSE\
+    --env SNOWFLAKE_ACCOUNT\
+    snowflake_llm
